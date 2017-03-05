3d58be8ee603753e56e6e35ac347e4a3728c9805
Chrisi Hendricks <hendricks.christina@gmail.com>
2017-03-05 13:12:59 -0500

    Adjust jquery and CSS so that mobile menu toggles on and off

--- js/navigation.js
+++ js/navigation.js
@@ -1,17 +1,5 @@
 $(document).ready(function(){
-	$('nav a.mobile_menu').on('click', function(){
-		var currentNavHeight = $('nav').height();
-		if(currentNavHeight < 5){
-
-			var newNavHeight = $('nav > ul').height + 15;
-			$('nav').animate({'height':newNavHeight+'px'},750);
-
-		}else{
-
-			$('nav').animate({'height':'0px'},750, function(){
-				$(this).removeAttr('style');
-			});
-
-		}
+	$( "nav a.mobile_menu" ).click(function() {
+  $( "nav" ).toggleClass( "menuOpen" );
 	})
 });
