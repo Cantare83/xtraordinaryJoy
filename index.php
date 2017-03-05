bc3159ea3a905ff42985760d0bcf196ae1934026
Chrisi Hendricks <hendricks.christina@gmail.com>
2017-03-05 14:36:19 -0500

    Add skeleton layout structure and comments to homepage

--- index.php
+++ index.php
@@ -1,40 +1,39 @@
 <?php get_header(); ?>
 
 <!-- Begin Main Content -->
-<section class="row">
-    <div class="twelve columns">
-      <!-- Begin Loop -->
-      <?php
-        if ( have_posts() ) {
-          while ( have_posts() ) {
-            the_post(); ?>
+<section class="row">  <!-- Intro Section -->
+    <div class="four columns">
 
-            <?php
-              if ( has_post_thumbnail() ) {
-                  the_post_thumbnail('thumbnail');
-              }
-            ?>
+    </div>
+    <div class="eight columns">
 
-            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-            <?php the_excerpt(); ?>
-            <a href="<?php the_permalink(); ?>">Read More</a>
+    </div>
+</section>
+<section class="row">  <!-- About Section -->
+    <div class="five columns">
 
-        <?php
-          } //end while
-        } //end if
-      ?>
-      <!-- Add the pagination function here -->
-                <div class="row">
-                    <div class="twelve columns">
-                      <div class="pagination">
-                        <?php next_posts_link('More Posts'); ?>
-                      </div>
-                      <div class="pagination">
-                        <?php previous_posts_link('Previous Posts'); ?>
-                      </div>
-                    </div>
-                </div>
-      <!-- End Loop -->
+    </div>
+    <div class="seven columns">
+
+    </div>
+</section>
+<section class="row">  <!-- Events Section -->
+    <div class="eight columns">
+
+    </div>
+    <div class="four columns">
+
+    </div>
+</section>
+<section class="row">  <!-- Resources Section -->
+    <div class="four columns">
+
+    </div>
+    <div class="three columns">
+
+    </div>
+    <div class="three columns">
+
     </div>
 </section>
 <!-- End Main Content -->
