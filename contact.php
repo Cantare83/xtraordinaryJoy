cdfc77c3b2214e53f1f33bdbb63fd0019e78cd7c
Andy DiBella <adibella@gmail.com>
2017-03-18 13:14:07 -0700

    add stylization tweaks to form and post

--- contact.php
+++ contact.php
@@ -2,27 +2,27 @@
 
 <?php get_header(); ?>
 
-    <section class="row">
+    <section class="row contactcontainer">
       <div class="contactcontainer">
-        <div class="six columns">
+          <div class="six columns">
 
-          <div class="contactimage">
-            <?php dynamic_sidebar('Contact Image Widget'); ?>
+              <div class="contactimage">
+                  <?php dynamic_sidebar('Contact Image Widget'); ?>
+              </div>
+
+              <div class="contactinfo">
+                  <?php dynamic_sidebar('Contact Info Widget'); ?>
+              </div>
+
           </div>
 
-          <div class="contactinfo">
-            <?php dynamic_sidebar('Contact Info Widget'); ?>
+          <div class="six columns">
+
+            <div class="contactform">
+                <?php dynamic_sidebar('Contact Form Widget'); ?>
+            </div>
+
           </div>
-
-        </div>
-
-        <div class="six columns">
-
-          <div class="contactform">
-            <?php dynamic_sidebar('Contact Form Widget'); ?>
-          </div>
-
-        </div>
       </div>
     </section>
 
